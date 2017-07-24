35e169c619ca7aa052f9685bd152ab6d74294bfd
Mihai Iftode <iftode.mihai@agilefreaks.com>
2017-07-24 14:00:17 +0300

    [#10] Fix mock route data store

--- presentation/src/mock/java/com/agilefreaks/gripit/data/repository/datasource/MockRouteDataStore.kt
+++ presentation/src/mock/java/com/agilefreaks/gripit/data/repository/datasource/MockRouteDataStore.kt
@@ -8,19 +8,18 @@ import java.util.*
 class MockRouteDataStore : RouteDataStore {
     override fun routeEntities(): Observable<Collection<RouteEntity>> {
         val calendar = Calendar.getInstance()
-        val dateFormat = SimpleDateFormat.getDateInstance()
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         val time = dateFormat.format(calendar.time)
         return Observable.fromArray(listOf(
-                RouteEntity(1, "Route 1", "placeholder.jpg", "6A", listOf("Easy"), time, "Mock Climber"),
-                RouteEntity(2, "Route 2", "placeholder.jpg", "6A", listOf("Medium"), time, "Mock Climber"),
-                RouteEntity(3, "Route 3", "placeholder.jpg", "6A", listOf("Hard"), time, "Mock Climber")))
+                RouteEntity(1, "Route 1", "placeholder.jpg", "6A", listOf("Easy"), time, "Mock Climber", listOf("")),
+                RouteEntity(2, "Route 2", "placeholder.jpg", "6A", listOf("Medium"), time, "Mock Climber", listOf("")),
+                RouteEntity(3, "Route 3", "placeholder.jpg", "6A", listOf("Hard"), time, "Mock Climber", listOf(""))))
     }
 
     override fun routeEntityDetails(routeId: Int): Observable<RouteEntity> {
         val calendar = Calendar.getInstance()
-        val dateFormat = SimpleDateFormat.getDateInstance()
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         val time = dateFormat.format(calendar.time)
-        return Observable.just(RouteEntity(1, "Route 1", "placeholder.jpg", "6A", listOf("Easy"), time, "Mock Climber"))
+        return Observable.just(RouteEntity(1, "Route 1", "placeholder.jpg", "6A", listOf("Easy"), time, "Mock Climber", listOf("")))
     }
-
 }
